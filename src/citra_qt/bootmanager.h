9c0b5503c9e8b73cb28ab00cf0ac80d17d6630d5
wwylele <wwylele@gmail.com>
2016-06-16 21:49:09 +0300

    Frontend: emulate motion sensor

--- src/citra_qt/bootmanager.h
+++ src/citra_qt/bootmanager.h
@@ -8,6 +8,7 @@
 #include <QGLWidget>
 #include <QThread>
 #include "common/emu_window.h"
+#include "common/motion_emu.h"
 #include "common/thread.h"
 
 class QKeyEvent;
@@ -153,6 +154,9 @@ private:
     int keyboard_id;
 
     EmuThread* emu_thread;
+
+    /// Motion sensors emulation
+    std::unique_ptr<Motion::MotionEmu> motion_emu;
 
 protected:
     void showEvent(QShowEvent* event) override;
--- src/common/vector_math.h
+++ src/common/vector_math.h
@@ -187,6 +187,18 @@ Vec2<T> operator*(const V& f, const Vec2<T>& vec) {
 
 typedef Vec2<float> Vec2f;
 
+template <>
+inline float Vec2<float>::Length() const {
+    return std::sqrt(x * x + y * y);
+}
+
+template <>
+inline float Vec2<float>::Normalize() {
+    float length = Length();
+    *this /= length;
+    return length;
+}
+
 template <typename T>
 class Vec3 {
 public:
@@ -388,6 +400,13 @@ inline float Vec3<float>::Length() const {
 template <>
 inline Vec3<float> Vec3<float>::Normalized() const {
     return *this / Length();
+}
+
+template <>
+inline float Vec3<float>::Normalize() {
+    float length = Length();
+    *this /= length;
+    return length;
 }
 
 typedef Vec3<float> Vec3f;
