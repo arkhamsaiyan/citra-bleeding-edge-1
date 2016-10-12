7db3d1ff85048958ed2c539e6f5ddcf3968c5a4a
Anon <makotech222@gmail.com>
2016-10-11 20:01:14 -0500

    InputCore Overhaul

--- src/citra_qt/bootmanager.h
+++ src/citra_qt/bootmanager.h
@@ -120,12 +120,9 @@ public:
     void keyPressEvent(QKeyEvent* event) override;
     void keyReleaseEvent(QKeyEvent* event) override;
 
-    void mousePressEvent(QMouseEvent* event) override;
-    void mouseMoveEvent(QMouseEvent* event) override;
-    void mouseReleaseEvent(QMouseEvent* event) override;
-
-    void ReloadSetKeymaps() override;
-
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
     void OnClientAreaResized(unsigned width, unsigned height);
 
     void InitRenderTarget();
